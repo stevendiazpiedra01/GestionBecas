9da9974ac30ebc0472de99a9cd1f0e7e30278c96
stevendiazpiedra01 <bdiazpiedra@uniminuto.edu.co>
2019-11-29 15:15:45 -0500

    Editando Mockup

--- src/app/php/usuarios_Mockup.php
+++ src/app/php/usuarios_Mockup.php
@@ -53,71 +53,50 @@
 </nav>
 
 <div class="container">
-<form>
-  <div class="form-row">
-    <div class="col-md-4 mb-3">
-      <label for="validationServer01">First name</label>
-      <input type="text" class="form-control is-valid" id="validationServer01" placeholder="First name" value="Mark" required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
-    </div>
-    <div class="col-md-4 mb-3">
-      <label for="validationServer02">Last name</label>
-      <input type="text" class="form-control is-valid" id="validationServer02" placeholder="Last name" value="Otto" required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
-    </div>
-    <div class="col-md-4 mb-3">
-      <label for="validationServerUsername">Username</label>
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="inputGroupPrepend3">@</span>
+    <form (submit)="addStudent(nom, ape, email, edad, tel, dir)">
+        <div class="form-row">
+            <div class="col-md-4 mb-3">
+                <label for="nombres">Nombres</label>
+                <input type="text" #nom class="form-control" id="nombres" placeholder="Nombres" required>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="apellidos">Apellidos</label>
+                <input type="text" #ape class="form-control" id="apellidos" placeholder="Apellidos" required>
+            </div>
+            <div class="col-md-4 mb-3">
+                <label for="emails">Email</label>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend2">@</span>
+                    </div>
+                    <input type="email" #email class="form-control" id="emails" placeholder="Email" aria-describedby="inputGroupPrepend2" required>
+                </div>
+            </div>
         </div>
-        <input type="text" class="form-control is-invalid" id="validationServerUsername" placeholder="Username" aria-describedby="inputGroupPrepend3" required>
-        <div class="invalid-feedback">
-          Please choose a username.
+        <div class="form-row">
+            <div class="col-md-3 mb-3">
+                <label for="edad">Edad</label>
+                <input type="text" #edad class="form-control" id="edad" placeholder="Edad" required>
+            </div>
+            <div class="col-md-3 mb-3">
+                <label for="tel">Telefono</label>
+                <input type="text" #tel class="form-control" id="tel" placeholder="Telefono" required>
+            </div>
+            <div class="col-md-6 mb-3">
+                <label for="dir">Direccion</label>
+                <input type="text" #dir class="form-control" id="dir" placeholder="Direccion" required>
+            </div>
         </div>
-      </div>
-    </div>
-  </div>
-  <div class="form-row">
-    <div class="col-md-6 mb-3">
-      <label for="validationServer03">City</label>
-      <input type="text" class="form-control is-invalid" id="validationServer03" placeholder="City" required>
-      <div class="invalid-feedback">
-        Please provide a valid city.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationServer04">State</label>
-      <input type="text" class="form-control is-invalid" id="validationServer04" placeholder="State" required>
-      <div class="invalid-feedback">
-        Please provide a valid state.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3">
-      <label for="validationServer05">Zip</label>
-      <input type="text" class="form-control is-invalid" id="validationServer05" placeholder="Zip" required>
-      <div class="invalid-feedback">
-        Please provide a valid zip.
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
-      <label class="form-check-label" for="invalidCheck3">
-        Agree to terms and conditions
-      </label>
-      <div class="invalid-feedback">
-        You must agree before submitting.
-      </div>
-    </div>
-  </div>
-  <button class="btn btn-primary" type="submit">Submit form</button>
-</form>
+        <div class="form-group">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                <label class="form-check-label" for="invalidCheck2">
+              Acepta registrar su informacion ?
+            </label>
+            </div>
+        </div>
+        <button class="btn btn-primary">Agregar Registro</button>
+    </form>
 <br>
 <input id="archivos" name="imagenes[]" type="file" multiple=true class="file-loading">
 	</body>
@@ -147,8 +126,8 @@
 	
 	});
 	
-	</script>
-</div>
+  </script>
+  
 <br>
 <footer class="footer bg-dark rounded-top text-center">
         <div class="container py-2">
